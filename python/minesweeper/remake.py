ea6efcd40e6861abb48883506f57e6974384ed2b
shinarit <tetra666@gmail.com>
2012-02-20 16:44:30 +0100

    remake started

---
+++ python/minesweeper/remake.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#########################################
+##  minesweeper with custom tiled grid ##
+##     author: tetra666@gmail.com      ##
+#########################################
+
+from Tkinter import *
+
+class Field:
+  def __init__(self, isbomb, isvisible):
+    self.isbomb = isbomb
+    self.isvisible = isvisible
+    self.bombs = -1
+    self.issigned = False
+
+  def isBomb(self):
+    return self.isbomb
+
+  def isVisible(self):
+    return self.isvisible
+
+  def isSigned(self):
+    return self.issigned
+
+  def countBombs(self):
+    if self.bombs == -1:
+      #calculate
+      pass
+    return self.bombs
+
+  #static method
+  #generates a grid from an abstract size value
+  #returns the "root" of the generated graph, it should be the middle element, but doesn't matter
+  @staticmethod
+  def buildGrid(size):
+    return None    
+
+  
+
