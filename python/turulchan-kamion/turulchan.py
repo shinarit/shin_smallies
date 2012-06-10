3646db1f5a2c66455843fc70ae2dd3605163edbc
herper <lolwut@citromail.hu>
2012-06-10 12:31:34 +0200

    rendes max fájlméret küldése

--- python/turulchan-kamion/turulchan.py
+++ python/turulchan-kamion/turulchan.py
@@ -8,7 +8,7 @@ import re
 import cookielib
 from util import *
 
-def Post(board, reply, _file, cookies, name, email, text, password = '', proxy = None):
+def Post(board, reply, _file, cookies, name, email, text, password = 'kamion', proxy = None, max_size = "2560000"):
   if reply=='0': return False
   opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
   if proxy:
@@ -16,7 +16,8 @@ def Post(board, reply, _file, cookies, name, email, text, password = '', proxy =
   opener.addheaders = [('User-agent', 'Mozilla/5.0')]
   #resp=opener.open(url)
   #a=resp.read()
-  params={'board':board, 'replythread':reply, 'MAX_FILE_SIZE':"1024000",  "email":'', "name":name, 'em':email, 'message':text, 'postpassword':password}
+  params={'board':board, 'replythread':reply, 'MAX_FILE_SIZE': max_size,  
+  "email":'', "name":name, 'em':email, 'message':text, 'postpassword':password}
   opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies),
                                 MultipartPostHandler)
   if proxy:
@@ -24,6 +25,14 @@ def Post(board, reply, _file, cookies, name, email, text, password = '', proxy =
   opener.addheaders = [('User-agent', 'Mozilla/5.0')]
   if (_file):
     params['imagefile']=open(_file, "rb")
-  else:
-    params['looong']=''
+  #else: 
+  #  params['looong']='' #<-- ez már nem lohere!
   return opener.open('http://turulchan.net/board.php', params).read()
+
+def GetMaxSize(board, cookies, proxy = None):
+  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
+  if proxy:
+    opener.add_handler(urllib2.ProxyHandler({'http':proxy}))
+  opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+  page = opener.open('http://turulchan.net/%s/'%board).read()
+  return page.split('<input type="hidden" name="MAX_FILE_SIZE" value="')[1].split('" />')[0]
